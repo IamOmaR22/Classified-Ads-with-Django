21ab65f4b22f84b4d9244b2e66eae5582f775d87
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-10 15:35:41 +0600

    Product and Category Models Created

--- product/models.py
+++ product/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Product(models.Model):
+
+    CONDITION_TYPE = (
+        ("New", "New"),
+        ("Used", "Used")
+    )
+
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500)
+    condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=5)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
