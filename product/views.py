f7da9941273e60bab70510d0e0bedfbfedb9502e
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-14 09:48:38 +0600

    Product Search Done

--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Product, ProductImages, Category
 from django.core.paginator import Paginator
 from django.db.models import Count
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,6 +14,16 @@ def productlist(request, category_slug=None):
     if category_slug:
         category = Category.objects.get(slug=category_slug)
         productlist = productlist.filter(category=category)
+
+    search_query = request.GET.get('q')
+    if search_query:
+        productlist = productlist.filter(
+            Q(name__icontains = search_query) |
+            Q(description__icontains = search_query) |
+            Q(condition__icontains = search_query) |
+            Q(brand__brand_name__icontains = search_query) |
+            Q(category__category_name__icontains = search_query)
+        )
 
     paginator = Paginator(productlist, 5) # Show 5 product per page
     page = request.GET.get('page')
