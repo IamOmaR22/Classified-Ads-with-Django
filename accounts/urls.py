b38a2a2590f3580641315af1804fa8cbb278c893
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-16 21:01:17 +0600

    Registration Done

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 # from . import views
 # from django.contrib.auth import login
 from django.contrib.auth import views as auth_views
+from . views import register
 
 urlpatterns = [
+    path('register/' , register , name='register') ,
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
--- classified_ads/urls.py
+++ classified_ads/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
